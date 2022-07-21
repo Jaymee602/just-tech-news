9a7036377264b4078e5d58ccf446f472c04e5f51
jam <jaymee602@gmail.com>
2022-07-20 18:20:25 -0700

    make comment model and routes

--- root/routes/api/post-routes.js
+++ root/routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const sequelize = require('../../config/connection')
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -17,6 +17,14 @@ router.get('/', (req, res) => {
         {
           model: User,
           attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         }
       ]
     })
@@ -45,6 +53,14 @@ router.get('/:id', (req, res) => {
       {
         model: User,
         attributes: ['username']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
       }
     ]
   })
